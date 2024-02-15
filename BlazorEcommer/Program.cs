ae1c3e12f3d7e664910b35423d463e0f4007106c
Amin <KahramAmin@gmail.com>
2024-02-15 12:08:16 +0330

    Razor component test and two way data binding

--- BlazorEcommer/Program.cs
+++ BlazorEcommer/Program.cs
@@ -1,4 +1,4 @@
-using BlazorEcommer;
+using BlazorEcommer_Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
---
+++ Ecommers.Core/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace Ecommers.Core.Models;
+
+public class Product
+{
+    public int ID { get; set; }
+    public string Name { get; set; }
+    public double Price { get; set; }
+    public bool IsActive { get; set; }
+}
