05ddaee194b3e2d3d7179aad35d99481fef51297
Amin <KahramAmin@gmail.com>
2024-02-16 17:29:09 +0330

    navigating to a url with navigation manger and setting parameter to accept int

--- BlazorEcoomer_Server/obj/Debug/net8.0/BlazorEcoomer_Server.AssemblyInfo.cs
+++ BlazorEcoomer_Server/obj/Debug/net8.0/BlazorEcoomer_Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorEcoomer_Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af926383cf4ecf291999cbd382ba279f431e04c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+526a5d06c4d0f6411a109a9682125820a8f3b674")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorEcoomer_Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorEcoomer_Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
